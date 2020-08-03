3e0365844266e3a8cac8a3896a5bde18dba73357
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2020-08-03 18:54:10 +0200

    first stack

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/product_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProductOverviewScreen(),
+        routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()},
+      ),
+    );
+  }
+}
