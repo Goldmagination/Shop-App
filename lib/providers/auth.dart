64bce52418b32bb436df87ee2f440e33883a0683
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2020-08-12 15:22:24 +0200

    error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
-
 import 'package:http/http.dart' as http;
+
+import '../models/http_exeption.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -10,27 +11,44 @@ class Auth with ChangeNotifier {
   String _userId;
 
   Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBTJyWDAXYjU0otEsejEARqbWwGLPTkhhA';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      const url =
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBTJyWDAXYjU0otEsejEARqbWwGLPTkhhA';
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> login(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBTJyWDAXYjU0otEsejEARqbWwGLPTkhhA';
-    final response = await http.post(url,
-        body: json.encode(
-            {'email': email, 'password': password, 'returnSecureToken': true}));
-    print(json.decode(response.body));
+    try {
+      const url =
+          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBTJyWDAXYjU0otEsejEARqbWwGLPTkhhA';
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true
+          }));
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
